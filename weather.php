43fb45d6f67c617a952233bba14ca047a664ef37
Lin Trieu <lin@Lins-MacBook-Pro.local>
2019-08-19 17:51:40 +0100

    Adds Weather class with empty methods

---
+++ weather.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+class Weather
+{
+    function __construct()
+    {
+
+    }
+
+    function random_outlook()
+    {
+
+    }
+
+    function stormy()
+    {
+
+    }
+}
