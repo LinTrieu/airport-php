34ca56a29851eedb62599ff15bc6e5231ec1ed6c
Lin Trieu <linnatrieu@gmail.com>
2019-08-31 10:47:23 +0100

    Refactors codebase and refactors

--- hanger.php
+++ hanger.php
@@ -31,7 +31,6 @@ class Hanger
         catch (Exception $e)
         {
             echo $e->getMessage();
-//            throw $e;
         }
     }
 
@@ -68,13 +67,13 @@ class Hanger
     }
 }
 
-$thursday = new Weather;
-$terminal_one = new Hanger($thursday, 2);
-$ryanair = new Plane;
-$easyjet = new Plane;
-$ba = new Plane;
-$terminal_one->land($ryanair);
-$terminal_one->land($easyjet);
-$terminal_one->takeoff($ryanair);
-var_dump('----------');
-print_r($terminal_one->planes);
+//$thursday = new Weather;
+//$terminal_one = new Hanger($thursday, 2);
+//$ryanair = new Plane;
+//$easyjet = new Plane;
+//$ba = new Plane;
+//$terminal_one->land($ryanair);
+//$terminal_one->land($easyjet);
+//$terminal_one->takeoff($ryanair);
+//var_dump('----------');
+//print_r($terminal_one->planes);
